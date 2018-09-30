23c9593a7d58edebda57abe9fde59a0b708c9a87
emil.huo <emil.huo@gmail.com>
2018-09-30 16:03:52 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -23,13 +23,12 @@ function onKeyDownHandler(e) {
   console.log(key);
   if (key === codes[index]) {
     index++;
-
- } else {
+    
+    if (index === codes.length) {
+      window.alert("Hurray!");
+      index = 0;
+    } else {
     index = 0;
-  }
-  if (index === codes.length) {
-    window.alert("Hurray!");
-   
   }
     
   }
