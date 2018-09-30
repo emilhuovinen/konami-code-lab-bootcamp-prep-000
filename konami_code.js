5bae0aea29c3fc8aab87626b921ca65d467e77cb
emil.huo <emil.huo@gmail.com>
2018-09-30 14:58:09 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,21 +14,26 @@ const codes = [
 
 
 function init() {
-  // your code here	  //
-  var index = 0
-  document.body.addEventListener('keydown', onKeyDownHandler);
-   function onKeyDownHandler(e) {
-    const key = parseInt(e.detail || e.which)
-    console.log('key', key)
-    if (key === code[index]) {
-      index++
-       if (index === code.length) {
-        alert("YOU DID IT!")
-        index = 0
-      }
-    } else {
-      index = 0
-    }
+let index = 0;
+document.body.addEventListener('keydown', onKeyDownHandler);
+
+
+function onKeyDownHandler(e) {
+  const key = e.key;
+  
+  if (key === codes[index]) {
+    index++;
+  
+  
+  if (index === codes.length) {
+    return alert("Hurray!")
+    index = 0;
+  } else {
+    index = 0;
   }
-}	
- init();
+    
+  }
+  
+}
+
+}
